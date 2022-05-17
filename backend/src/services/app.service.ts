1173c8bface08425ac1aed9057c53ea2d7132b0e
RTae <natthanan.b@dia.co.th>
2022-05-17 22:37:36 +0700

    add : new feature

--- backend/src/services/app.service.ts
+++ backend/src/services/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Fishstop!';
+    return 'Hello Fishstop from CPE393!';
   }
 }
